3474454486646a4e1cbb0e5d8d9831d431e9d6b5
bobthehacker101 <75433097+bobthehacker101@users.noreply.github.com>
2020-12-03 15:14:03 +0100

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,2 +1,1 @@
-document.write ("This is remote text via xss.js located at xss.rocks " + document.cookie);
-alert ("This is remote text via xss.js located at xss.rocks " + document.cookie);
+window.open("http://www.google.com")
